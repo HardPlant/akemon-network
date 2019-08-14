35c920c14b4fc3e147bc763403ad5a14122d8aad
HardPlant <abc7988se@naver.com>
2019-08-14 09:41:04 +0900

    not supporting form node

--- js/login/api.js
+++ js/login/api.js
@@ -1,14 +1,14 @@
 var firebase = require("firebase");
-var fs = require("fs")
-eval(fs.readFileSync("login/config.js")+"");
 
 async function login() {
+    firebase.initializeApp(firebaseConfig);
+
     var provider = new firebase.auth.GoogleAuthProvider();
     var resultToken = await firebase.auth().signInWithPopup(provider).then(function(result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
         return token;
     });
-    
+
     return resultToken;
 }
--- js/login/api.unit.test.js
+++ js/login/api.unit.test.js
@@ -1,8 +1,8 @@
-eval(require("fs").readFileSync("login/api.js")+"");
+var fs = require("fs");
+eval(fs.readFileSync("login/config.js")+"");
+eval(fs.readFileSync("login/api.js")+"");
 
-it("prompts login", function() {
-    try {
-        var token = await login();
-        expect(token).not.toBe(undefined);
-    }
+it("prompts login", async function() {
+    var token = await login();
+    expect(token).not.toBe(undefined);
 });
