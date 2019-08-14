e292e4d16ad8fb0c05df05b86c538ec2d1cffa60
HardPlant <abc7988se@naver.com>
2019-08-14 11:00:32 +0900

    redis deployed

--- redis/redis.js
+++ redis/redis.js
@@ -1,5 +1,8 @@
 var redis = require("redis"),
-    client = redis.createClient();
+    client = redis.createClient({
+        host: "redis-service",
+        port: 6379
+    });
 
 // if you'd like to select database 3, instead of 0 (default), call
 // client.select(3, function() { /* ... */ });
