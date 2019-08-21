0ede62ede675ed83c4231ba62c9f361098b49d44
HardPlant <abc7988se@naver.com>
2019-08-21 13:25:22 +0900

    matchmaker project

---
+++ lobby/matchmaker/matchmaker-server/src/main/java/io/hardplant/MatchmakerServerApplication.java
@@ -0,0 +1,13 @@
+package io.hardplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchmakerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchmakerServerApplication.class, args);
+	}
+
+}
